2fd3306065cac47fe40611a417e36354d7d901ae
kkorvel <koit.korvel@gmail.com>
2016-08-07 20:55:57 +0300

    Add files via upload
    update

--- puur.py
+++ puur.py
@@ -3,26 +3,35 @@ import sys
 #from PyMata.pymata import PyMata
 from PyQt4.QtGui import QApplication, QMainWindow, QPushButton,QVBoxLayout, QWidget, QTextEdit
 app = QApplication([])
-
+button_convert = QPushButton("Convert order")
 mainwindow = QMainWindow()
 mainwindow.setWindowTitle("Puuri koordinaadid")
 
 vbox = QVBoxLayout()
 vbox.addStretch(1)
 
-textedit = QTextEdit("box 1")
-textedit2 = QTextEdit("box 2")
-textedit3 = QTextEdit("box 3")
-
+textedit = QTextEdit("First part")
+textedit2 = QTextEdit("Second part")
+textedit3 = QTextEdit("Result")
 
 vbox.addWidget(textedit)
 vbox.addWidget(textedit2)
 vbox.addWidget(textedit3)
+vbox.addWidget(button_convert)
+
 # This is some silly Qt-ism
 container = QWidget()
 container.setLayout(vbox)
 mainwindow.setCentralWidget(container)
 mainwindow.show()
 
+
+def convert_handler(convert):
+	value = textedit2.value()
+	
+    
+
+
+button_convert.clicked.connect(convert_handler)
 
 sys.exit(app.exec_()) # This is basically infinite loop here, it blocks everything else!
