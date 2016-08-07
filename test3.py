d7c758a679a6f0506a518787fa6767e614a722bd
kkorvel <koit.korvel@gmail.com>
2016-08-07 09:40:10 +0300

    Add files via upload
    test box

---
+++ test3.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        
+        title = QtGui.QLabel('First part')
+        author = QtGui.QLabel('Second part')
+        review = QtGui.QLabel('Correct order')
+
+        titleEdit = QtGui.QTextEdit()
+        authorEdit = QtGui.QTextEdit()
+        reviewEdit = QtGui.QTextEdit()
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+        
+        self.setLayout(grid) 
+        
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Puuri koordinaadid!')    
+        self.show()
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
