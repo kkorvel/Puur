a747003597cb90b4d6f021f7cc31ba7e2d0618f5
kkorvel <koit.korvel@gmail.com>
2016-08-07 10:37:23 +0300

    Add files via upload
    3 boxi

--- puur.py
+++ puur.py
@@ -12,10 +12,12 @@ vbox.addStretch(1)
 
 textedit = QTextEdit("box 1")
 textedit2 = QTextEdit("box 2")
+textedit3 = QTextEdit("box 3")
 
 
 vbox.addWidget(textedit)
 vbox.addWidget(textedit2)
+vbox.addWidget(textedit3)
 # This is some silly Qt-ism
 container = QWidget()
 container.setLayout(vbox)
